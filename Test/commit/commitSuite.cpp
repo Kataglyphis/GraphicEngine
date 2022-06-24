f3df3b479b28e9dabae9a22b214f983074436596
Jonas Heinle <jonasheinle@googlemail.com>
2022-06-24 13:41:19 +0200

    added test for renderer
    added test for renderer

--- Test/commit/commitSuite.cpp
+++ Test/commit/commitSuite.cpp
@@ -9,20 +9,57 @@
 #include "OpenGLRendererConfig.hpp"
 #include "GameObject.hpp"
 
-// Demonstrate some basic assertions.
-TEST(HelloTest1, BasicAssertions) {
+#include "Renderer.hpp"
+#include "DebugApp.hpp"
+#include "GUI.hpp"
+#include "LoadingScreen.hpp"
 
-	// Expect two strings not to be equal.
-	EXPECT_STRNE("hello", "world");
-	// Expect equality.
-	EXPECT_EQ(7 * 6, 42);
+// Demonstrate some basic assertions.
+TEST(RendererTest, BasicSetup) {
+
+	Renderer renderer(1200,800);
+
+	GLint window_width = 1200;
+    GLint window_height = 800;
+
+    // make sure ti initialize window first
+    // this will create opengl context!
+    std::shared_ptr<Window> main_window =
+        std::make_shared<Window>(window_width, window_height);
+
+    DebugApp debugCallbacks;
+
+    Renderer renderer(window_width, window_height);
+
+    GUI gui;
+    gui.init(main_window);
+
+    LoadingScreen loading_screen;
+    loading_screen.init();
+
+    std::shared_ptr<Camera> main_camera = std::make_shared<Camera>();
+
+    std::shared_ptr<Scene> scene =
+        std::make_shared<Scene>(main_camera, main_window);
+
+    glm::mat4 projection_matrix = glm::perspectiveFov(
+        glm::radians(main_camera->get_fov()), (GLfloat)window_width,
+        (GLfloat)window_height, main_camera->get_near_plane(),
+        main_camera->get_far_plane());
+
+    GLfloat delta_time = 0.0f;
+    GLfloat last_time = 0.0f;
+
+    GLfloat now = (float)glfwGetTime();
+    delta_time = now - last_time;
+    last_time = now;
 
 }
 
 TEST(ObjLoaderTest, blob)
 {
 	
-	std::vector<std::shared_ptr<GameObject>> gameObjects;
+    std::vector<std::shared_ptr<GameObject>> gameObjects;
 
   glm::vec3 sponza_offset = glm::vec3(0.f, 0.0f, 0.0f);
   GLfloat sponza_scale = 10.f;
@@ -40,4 +77,5 @@ TEST(ObjLoaderTest, blob)
       modelFile.str(), sponza_offset, sponza_scale, sponza_rot);
   gameObjects.push_back(sponza);
   ASSERT_EQ(static_cast<uint32_t>(gameObjects.size()), 1);
+
 }
