74522d60757e47f0a247700ff28e1f85d9237dcc
Jonas Heinle <jonasheinle@googlemail.com>
2022-07-14 13:29:56 +0200

    fix

--- Src/scene/sky_box/SkyBox.hpp
+++ Src/scene/sky_box/SkyBox.hpp
@@ -4,11 +4,11 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <memory>
 
 #include "GlobalValues.hpp"
 #include "Mesh.hpp"
 #include "ShaderProgram.hpp"
-
 class SkyBox {
  public:
   SkyBox();
